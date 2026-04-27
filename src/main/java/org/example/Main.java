ce208d659f09087b499b345f6b272f13c41dd7ab
Maya <maya.mhiles79@gmail.com>
2026-04-27 11:24:53 +0200

    Setup Logviewer

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.example.view.SwingUI;
+
+import javax.swing.*;
+
+public class Main {
+    static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new SwingUI());
+
+
+    }
+}
---
+++ src/main/java/org/example/logger/LogEintrag.java
@@ -0,0 +1,4 @@
+package org.example.logger;
+
+public record LogEintrag(int zeilennummer, String level, String nachricht) {
+}
