f4163149a3df415d5b8cc11aa876841282ef8678
Maya <maya.mhiles79@gmail.com>
2026-04-28 22:18:48 +0200

    Add Javadoc comments

---
+++ src/main/java/org/example/logger/LogReader.java
@@ -0,0 +1,16 @@
+package org.example.logger;
+
+import java.io.File;
+import java.util.List;
+
+public class LogReader {
+    /**
+     * Liest die Logdatei ein ung gibt die Einträge zurück
+     *
+     * @param file die einzulesende Datei
+     * @return Liste der Logeinträge
+     */
+}
+    public List<String> readLogs(File file) {
+
+    }
